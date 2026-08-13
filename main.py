85b97c795a121143cbfb1f8b54a3a3d6f2746ab6
fadz <ahfadz.jelliut@gmail.com>
2026-08-13 21:28:38 +0800

    change file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import profile
 import json
 
 with open("data/profile.json","r",encoding="utf-8") as file:
-    profile = json.load(file)
+    profil = json.load(file)
 
-print(profile[""])
+print(profil[""])
